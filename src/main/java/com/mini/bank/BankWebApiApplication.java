d35a025fc54dfd342b8bae0d1096d774f81bafc4
bob <robertsarfo@outlook.com>
2021-10-14 13:38:28 +0000

    init

---
+++ src/main/java/com/mini/bank/BankWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.mini.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankWebApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankWebApiApplication.class, args);
+    }
+
+}
